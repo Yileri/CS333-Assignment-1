7f00f59b5071888facda04cb6c40293137f2a831
Yileri <yigit.ileri@ozu.edu.tr>
2023-04-09 22:58:56 +0300

    changed adjacency list to adjacency set in order to remove duplicates

--- KidneyDonation/python/donorcycle.py
+++ KidneyDonation/python/donorcycle.py
@@ -2,7 +2,7 @@
 def isInCycle(match_scores,donor_friends,query):
     # create an array for recipient nodes and adjacent nodes as arrays of each node
     node_count = len(match_scores[0])
-    nodes = [[] for x in range(node_count)]
+    nodes = [set() for x in range(node_count)]
 
     donor_count = len(match_scores)
 
@@ -12,7 +12,7 @@ def isInCycle(match_scores,donor_friends,query):
             if match_scores[row][col] >= 60:
                 donor_id = row
                 recipient_id = donor_friends[donor_id]
-                nodes[recipient_id].append(col)
+                nodes[recipient_id].add(col)
     print(nodes)
     return False
 
