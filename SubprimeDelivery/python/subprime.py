ed78419b5cc3cd524addfb977dcba3e1d39022af
Yileri <yigit.ileri@ozu.edu.tr>
2023-04-11 18:12:27 +0300

    initialize nodes array that will change throughout the process

--- SubprimeDelivery/python/subprime.py
+++ SubprimeDelivery/python/subprime.py
@@ -3,10 +3,18 @@ import heapq as pq
 def subprime_path(capacity_graph, load_graph, start, end):
     final_graph = [[] for x in range(len(capacity_graph))]
 
+    # compute ratios for edges and fill the graph with it
     for city in range(len(capacity_graph)):
         for truck in range(len(capacity_graph[city])):
-            final_graph[city].append((load_graph[city][truck][0], (load_graph[city][truck][1]/capacity_graph[city][truck][1])))
+            final_graph[city].append((load_graph[city][truck][0], (load_graph[city][truck][1]/capacity_graph[city][truck][1]) * 100))
 
+    cost_graph = {}
+    for city in range(len(final_graph)):
+        # make the default percentage higher than possible which is 100
+        # and set initial way to a node as empty
+        cost_graph[city] = {"cost": 105, "way": []}
+
+    print(cost_graph)
     print(load_graph)
     print(capacity_graph)
     print(final_graph)
